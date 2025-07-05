739e57be1c7b08773347c64dea8dfe6ec5b00f7b
vikkastaneja <vikkastaneja@users.noreply.github.com>
2025-07-04 18:03:06 -0700

    Fix existing unit tests

--- src/main.py
+++ src/main.py
@@ -31,9 +31,6 @@ def read_user(user_id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-
-app = FastAPI()
-
 UPLOAD_DIR = "uploaded_photos"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
